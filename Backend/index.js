35d374d009ac8cadbf68528725a32791c1c7a291
Nelson Pacheco <nelson.pacheco@tecinco.cl>
2022-08-09 12:09:03 -0400

    Prueba push

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ const port = 3000
 mongoose.connect('mongodb://127.0.0.1:27017/api_rest_blog').then(()=>{
     console.log('La Conexion a la BD se ha realiado con exito!!!')
 //Crear Servidor y poner a escuchar peticiones HTTP 
-
+//prueba CAmbio
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 })
